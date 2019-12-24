c39458cc77a5b26207b43f38fe5fa6d80c1a2cfd
apoorv <vappor.washmade@gmail.com>
2019-12-24 12:47:33 +0530

    unfinished December Problems

---
+++ src/main/java/org/apoorv/progfun/NumberParser.java
@@ -0,0 +1,9 @@
+package org.apoorv.progfun;
+
+public class NumberParser {
+    private static String[] words = {};
+
+    public static int parse(String num) {
+
+    }
+}
---
+++ src/main/java/org/apoorv/progfun/PrintDate.java
@@ -0,0 +1,47 @@
+package org.apoorv.progfun;
+
+import java.util.*;
+
+public class PrintDate {
+    private static final int[] numOfDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    private static final String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please enter an integer from 1 to 365 (366 for leap years): ");
+        int num = in.nextInt();
+        System.out.print("In which year? ");
+        int year = in.nextInt();
+        boolean isLeapYear = false;
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                isLeapYear = true;
+            }
+        } else if (year % 4 == 0) {
+            isLeapYear = true;
+        }
+        String res = getDate(num, isLeapYear);
+        System.out.println(res);
+    }
+
+    public static String getDate(int day, boolean leapYear) {
+        if (leapYear) {
+            numOfDays[1]++;
+            if (day > 366) {
+                day %= 366;
+            }
+        } else {
+            if (day > 365) {
+                day %= 365;
+            }
+        }
+        for (int i = 0; i < numOfDays.length; i++) {
+            if (day > numOfDays[i]) {
+                day -= numOfDays[i];
+            } else {
+                return months[i] + " " + day;
+            }
+        }
+        return "Some error occurred. Please try again.";
+    }
+}
