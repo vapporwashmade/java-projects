1f089526c800966d4d0b0764bebf2be2238b6198
apoorv <vappor.washmade@gmail.com>
2020-01-19 17:43:39 +0530

    Finished the transpose of a matrix problem

---
+++ src/main/java/org/apoorv/progfun/Determinant.java
@@ -0,0 +1,40 @@
+package org.apoorv.progfun;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Determinant {
+    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in).useDelimiter("\n");
+//        System.out.print("How many rows/columns in the matrix (2-5)? ");
+//        int rows = in.nextInt();
+//        double[][] matrix = new double[rows][];
+//        for (int i = 0; i < rows; i++) {
+//            System.out.print("Print row " + (i+1) + " separated by spaces: ");
+//            String[] row = in.next().split(" ");
+//            double[] a = new double[rows];
+//            for (int j = 0; j < row.length; j++) {
+//                a[j] = Double.parseDouble(row[j]);
+//            }
+//            matrix[i] = a;
+//        }
+//        System.out.println(Arrays.deepToString(matrix));
+//        System.out.println(determinant(matrix));
+        System.out.println(determinant(new double[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}));
+    }
+
+    public static double determinant(double[][] a) {
+        // Base Case
+        if (a[0].length == 2) {
+            return a[0][0] * a[1][1] - a[0][1] * a[1][0];
+        }
+        // Recursive Case
+        for (int i = 0; i < a.length; i++) {
+
+        }
+        return 1;
+    }
+
+//    public static int[][] exclude(int[][] )
+}
---
+++ src/main/java/org/apoorv/progfun/Transpose.java
@@ -0,0 +1,35 @@
+package org.apoorv.progfun;
+
+import java.util.Arrays;
+
+public class Transpose {
+    /**
+     * A matrix, as you may know, is a tabular arrangement of numbers (or other things). An mxn (m by n) matrix has
+     * m rows, each containing n numbers, or, it has m rows and n columns. As an example, here is a 2x3 matrix:
+     * 1 2 4
+     * 3 6 9
+     *
+     * An mxn matrix can be represented as an int[][] in Java.
+     *
+     * The transpose of a given matrix M is defined as another matrix N such that rows of M become columns of N.
+     * Thus, if M has m rows and n columns, N, its transpose, has n rows and m columns.
+     * @param a
+     * @return
+     */
+    public static final double[][] transpose(double[][] a) {
+        int m = a.length;
+        int n = a[0].length;
+        double[][] transpose = new double[n][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                transpose[j][i] = a[i][j];
+            }
+        }
+        return transpose;
+    }
+
+    public static void main(String[] args) {
+        double[][] a = {{1, 2, 3}, {4, 5, 6}};
+        System.out.println(Arrays.deepToString(transpose(a)));
+    }
+}
